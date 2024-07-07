a8c436e10dcc509d691564b6318f8befd5f6c5ec
addbad618 <addbad618@gmail.com>
2024-07-07 11:19:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandPrimary: "#4CAF4F",
+        brandPrimaryDark: "#3D8C3F",
+        neutralDGrey: "#4D4D4D",
+        neutralGrey: "#717171",
+        neutralSilver: "#F5F7FA",
+        gray900: "#18191F",
+        neutralBlack: "#263238",
+        darkh1: "#E9E9EA",
+        darkh2: "#A5A5A5",
+        darkbg: "#232323",
+        darkbg2: "#111827",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
